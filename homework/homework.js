0da0ae5adf35b8f988b8f91726dbaa39d27e8151
PavelGora <spec4web@gmail.com>
2017-01-22 07:33:08 +0300

    class Product is extracted to separate file

--- homework/homework.js
+++ homework/homework.js
@@ -1,10 +1,5 @@
 "use strict";
-class Product {
-    constructor(price, type) {
-        this._price = price;
-        this._type = type;
-    }
-}
+var Product = require("./product");
 
 const ProductType = {
     GROCERIES: "Groceries",
---
+++ homework/product.js
@@ -0,0 +1,8 @@
+"use strict";
+class Product {
+    constructor(price, type) {
+        this._price = price;
+        this._type = type;
+    }
+}
+module.exports = Product;
